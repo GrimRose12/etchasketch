a524cdf8708a4dbcabe4101fc590120415ba50e0
Core <carljeffersonlay@gmail.com>
2023-07-24 17:36:48 +0800

    Change to 16x16 grid

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector('.container');
 
-for (let i = 0; i < 4; i++){
+for (let i = 0; i < 16; i++){
     const row = document.createElement('div');
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 16; j++) {
         const gridContent = document.createElement('div');
         gridContent.id = i.toString()+j.toString();
         // gridContent.textContent = i.toString()+j.toString();
